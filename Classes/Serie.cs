2ff90b498f351e52c32634ee012124aa5d95435b
Liandro dos Anjos <oliverliandro@gmail.com>
2022-04-25 23:20:11 -0300

    Ajustes nas classes iniciais

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -1,7 +1,35 @@
+using System.Text;
+using Enums;
+
 namespace Classes
 {
-    public class Serie
+    public class Serie : EntidadeBase
     {
-        
+        public Serie(int id, E_Genero genero, string titulo, string descricao, int ano, bool excuido)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+            Excuido = excuido;
+        }
+
+        private E_Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excuido { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Gênero: {Genero}");
+            sb.AppendLine($"Titulo: {Titulo}");
+            sb.AppendLine($"Descrição: {Descricao}");
+            sb.AppendLine($"Ano de Início: {Ano}");
+
+            return sb.ToString();
+        }
     }
 }
--- Enums/E_Genero.cs
+++ Enums/E_Genero.cs
@@ -2,6 +2,18 @@ namespace Enums
 {
     public enum E_Genero
     {
-
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+        Ficcao_Cientifica = 9,
+        Musical = 10,
+        Romance = 11,
+        Suspense = 12,
+        Terror = 13
     }
 }
